4952be8b91ae7b156aff6cc9a8af38ad0e578a26
Daniel Ko <dankomong@gmail.com>
2019-03-18 01:27:21 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,2 +1,23 @@
 INSERT INTO bears (name, age, gender, color, temperament, owner_id)
 VALUES ("Mr. Chocolate", 1 , " boy", "black", "angry", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, owner_id)
+VALUES ("Rowdy", 1 , " boy", "black", "angry", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, owner_id)
+VALUES ("Tabitha", 1 , " boy", "black", "angry", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, owner_id)
+VALUES ("Sergeant Brown", 1 , " boy", "black", "angry", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, owner_id)
+VALUES ("Melissa", 1 , " boy", "black", "angry", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, owner_id)
+VALUES ("Grinch", 1 , " boy", "black", "angry", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, owner_id)
+VALUES ("Wendy", 1 , " boy", "black", "angry", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, owner_id)
+VALUES (NULL, 1 , " boy", "black", "angry", 1);
