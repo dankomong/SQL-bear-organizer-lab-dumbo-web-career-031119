51dacb58ff09cbc06e54ac58b9ad6f0601ea6262
Daniel Ko <dankomong@gmail.com>
2019-03-18 01:27:53 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,23 +1,23 @@
-INSERT INTO bears (name, age, gender, color, temperament, owner_id)
+INSERT INTO bears (name, age, gender, color, temperament, alive)
 VALUES ("Mr. Chocolate", 1 , " boy", "black", "angry", 1);
 
-INSERT INTO bears (name, age, gender, color, temperament, owner_id)
+INSERT INTO bears (name, age, gender, color, temperament, alive)
 VALUES ("Rowdy", 1 , " boy", "black", "angry", 1);
 
-INSERT INTO bears (name, age, gender, color, temperament, owner_id)
+INSERT INTO bears (name, age, gender, color, temperament, alive)
 VALUES ("Tabitha", 1 , " boy", "black", "angry", 1);
 
-INSERT INTO bears (name, age, gender, color, temperament, owner_id)
+INSERT INTO bears (name, age, gender, color, temperament, alive)
 VALUES ("Sergeant Brown", 1 , " boy", "black", "angry", 1);
 
-INSERT INTO bears (name, age, gender, color, temperament, owner_id)
+INSERT INTO bears (name, age, gender, color, temperament, alive)
 VALUES ("Melissa", 1 , " boy", "black", "angry", 1);
 
-INSERT INTO bears (name, age, gender, color, temperament, owner_id)
+INSERT INTO bears (name, age, gender, color, temperament, alive)
 VALUES ("Grinch", 1 , " boy", "black", "angry", 1);
 
-INSERT INTO bears (name, age, gender, color, temperament, owner_id)
+INSERT INTO bears (name, age, gender, color, temperament, alive)
 VALUES ("Wendy", 1 , " boy", "black", "angry", 1);
 
-INSERT INTO bears (name, age, gender, color, temperament, owner_id)
+INSERT INTO bears (name, age, gender, color, temperament, alive)
 VALUES (NULL, 1 , " boy", "black", "angry", 1);
