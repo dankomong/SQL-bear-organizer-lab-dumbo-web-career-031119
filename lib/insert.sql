530eed68da3e062a5599270e4058e48c19e3cf4f
Daniel Ko <dankomong@gmail.com>
2019-03-18 01:29:28 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,4 +1,5 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 1 , " boy", "black", "angry", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Mr. Chocolate", 1 , " boy", "black", "angry", true);
 
 INSERT INTO bears (name, age, gender, color, temperament, alive)
 VALUES ("Rowdy", 1 , " boy", "black", "angry", true);
